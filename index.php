d6e3dc10d434b347c4a9bef18e55496b4a1e3f98
centrinav <almendarizu.vanessa@gmail.com>
2016-02-15 18:34:12 -0500

    Primer commit
    Commit master

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Proyecto Final - Aplicaciones Web</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/landing-page.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <h1><a href="index.php"><img src="img/logo/gelatosferaSinFondo.png" height="90px" width="160px"></a></h1>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="http://www.facebook.com/gelatosfera/"><img src="img/logo/fb.png" height="50" width="50"></a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/gelatosfera/"><img src="img/logo/instag.png" height="50" width="50"></a>
+                    </li>
+                    <li>
+                        <a href="login.php"><button class="btn-success" id="signin" name="signin"> SIGN IN</button></a>
+                    </li>
+                </ul>
+            </div>
+            
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+
+    <!-- Header -->
+    <a name="about"></a>
+    <div class="intro-header">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.intro-header -->
+    
+    <br>
+    <br>
+    <br>
+    
+    
+    <!-- Contenido -->
+    
+     <div class="container-fluid">
+        <div class="row no-space">
+          <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/cherry.jpg" height="180" width="280" alt="" class="img-responsive"></a></div>
+          </div>
+          <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/chicle2.jpg" height="180" width="280"  alt="" class="img-responsive"></a></div>
+          </div>
+          <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/chocolate.jpg" height="180" width="280"  alt="" class="img-responsive"></a></div>
+          </div>
+          <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/frutimora.jpg" height="180" width="280"  alt="" class="img-responsive"></a></div>
+          </div>
+          <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/kibana.jpg" height="180" width="280"  alt="" class="img-responsive"></a></div>
+          </div>
+          <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/mandarina.jpg" height="180" width="280"  alt="" class="img-responsive"></a></div>
+          </div>
+          <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/mango.jpg" height="180" width="280"  alt="" class="img-responsive"></a></div>
+          </div>
+          <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/mix.jpg" height="180" width="280"  alt="" class="img-responsive"></a></div>
+          </div>
+            <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/natural.jpg" height="180" width="280"  alt="" class="img-responsive"></a></div>
+          </div>
+            <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/nutella.jpg" height="180" width="280"  alt="" class="img-responsive"></a></div>
+          </div>
+            <div class="col-sm-4 col-md-3">
+            <div class="box"><a href="#" title=""><img src="img/sabores/pi%C3%B1a.jpg" height="180" width="280"  alt="" class="img-responsive"></a></div>
+          </div>
+        </div>
+      </div>
+    
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="copyright text-muted small">Copyright &copy; Gelatosfera 2015.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
